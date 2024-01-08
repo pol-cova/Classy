663d5cd3ecc2c080f602d45d6d909341d8ae5314
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-01-08 02:11:22 -0600

    Add user logout functionality and signup form

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import index, home, user_login
+from .views import index, home, user_login, user_signup, user_logout
 
 urlpatterns = [
     path('', index , name='index'),
     path('my/', home, name='home'),
-    # test login route
-    path('login/', user_login, name='login')
+    path('login/', user_login, name='login'),
+    path('signup/', user_signup, name='signup'),
+    path('logout/', user_logout, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # import forms
 from .forms import SignUpForm, LoginForm
@@ -12,9 +12,13 @@ def index(request):
     else:
         form = LoginForm()
         return render(request, 'index.html', {'form': form})
-    
+
+# dashboard
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+    else:
+        return render(request, 'index.html', {'message': 'Inicia sesión para continuar...'})
 
 def user_login(request):
     if request.method == 'POST':
@@ -29,6 +33,21 @@ def user_login(request):
                 # login user
                 login(request, user)
                 return redirect('home')
+            else:
+                return render(request, 'index.html', {'message': 'Usuario o contraseña incorrectos'})
         else:
             return redirect('index')
 
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            # save user
+            form.save()
+            return render(request, 'index.html', {'message': 'Usuario creado exitosamente'})
+        else:
+            return redirect('index')
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
