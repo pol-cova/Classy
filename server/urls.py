3fa5b01b9fd3dbe17539a83c5fcd0a17fd843a09
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-01-15 02:35:36 -0600

    Add studyTimer functionality and update navigation links

--- server/urls.py
+++ server/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),# new
     path('todo/', include('todo.urls')),# new
+    path('timer/', include('studyTimer.urls')),# new
 ]
--- studyTimer/views.py
+++ studyTimer/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def studyTimer(request):
+    return render(request, 'timer.html')
