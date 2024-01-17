4aba14e67cdc228f9c0c285f0f72fa0bebd78ad3
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-01-16 22:45:51 -0600

    Add Post and Comment models to social app

--- social/models.py
+++ social/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Post
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='dislikes', blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.content[:20]}"
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+    def total_dislikes(self):
+        return self.dislikes.count()
+    
+# Comment
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='comment_dislikes', blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.content[:20]}"
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+    def total_dislikes(self):
+        return self.dislikes.count()
