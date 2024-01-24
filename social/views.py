53d4c53d2c7ded160aca67330525a92c75c04e36
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-01-24 14:10:54 -0600

    Add login_required decorator to post_list view

--- social/views.py
+++ social/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created')
+    
