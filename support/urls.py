f372f853a72ec70a99aa90d21ce8f73c6c72c1a4
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-02-11 03:58:41 -0600

    Update UI buttons and links

--- support/urls.py
+++ support/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import support_home, support_save, support_tickets, complet_ticket, delete_ticket
+from .views import support_home, support_save, complet_ticket, delete_ticket, terms, contact, about, admin_dash
 
 urlpatterns = [
     path('', support_home, name='support_home'),
+    path('dash/<str:token>/', admin_dash, name='admin_dash'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('terms-conditions/', terms, name='terms'),
     path('save-report/', support_save, name='support_save'),
-    path('all/tickets/', support_tickets, name='support_tickets'),
     path('complete/ticket/<int:ticket_id>/', complet_ticket, name='complet_ticket'),
     path('delete/ticket/<int:ticket_id>/', delete_ticket, name='delete_ticket'),
 ]
--- support/views.py
+++ support/views.py
@@ -2,15 +2,42 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Ticket
 from .forms import TicketForm
-
-
+from django.contrib.auth.models import User
+from social.models import Post, Comment
 # Create your views here.
+# admin dash
+def admin_dash(request, token):
+    if token == 'admin':
+        context = {
+            'tickets': Ticket.objects.all(),
+            'total_users': User.objects.all().count(),
+            'total_posts': Post.objects.all().count(),
+            'total_comments': Comment.objects.all().count(),
+            'total_tickets': Ticket.objects.all().count(),
+        }
+        return render(request, 'support_admin.html', context)
+    else:
+        return redirect('home')
+
+# terms and conditions
+def terms(request):
+    return render(request, 'terms.html')
+
+# about
+def about(request):
+    return render(request, 'about.html')
 
 def support_home(request):
-    contex = {
+    context = {
         'form': TicketForm()
     }
-    return render(request, 'support.html', contex)
+    return render(request, 'support.html', context)
+
+def contact(request):
+    context = {
+        'form': TicketForm()
+    }
+    return render(request, 'contact.html', context)
 
 def support_save(request):
     if request.method == 'POST':
@@ -18,39 +45,30 @@ def support_save(request):
         if form.is_valid():
             form.save()
             form = TicketForm()
-            contex = {
+            context = {
                 'form': form,
                 'message': 'Tu reporte se ha creado correctamente, pronto nos pondremos en contacto contigo.'
             }
-            return render(request, 'support.html', contex)
+            return render(request, 'contact.html', context)
         else:
-            contex = {
+            context = {
                 'form': form,
                 'message': 'Los datos ingresados no son válidos'
             }
-            return render(request, 'support.html', contex)
+            return render(request, 'support.html', context)
     else:
         return support_home(request)
     
-@login_required
-def support_tickets(request):
-    tickets = Ticket.objects.all()
-    context = {
-        'tickets': tickets
-    }
-    return render(request, 'support_tickets.html', context)
 
 # complete tickets
-@login_required
 def complet_ticket(request, ticket_id):
     ticket = get_object_or_404(Ticket, id=ticket_id)
     ticket.status = True
     ticket.save()
-    return redirect('support_tickets')
+    return redirect('admin_dash', 'admin')
 
 # delete tickets
-@login_required
 def delete_ticket(request, ticket_id):
     ticket = get_object_or_404(Ticket, id=ticket_id)
     ticket.delete()
-    return redirect('support_tickets')
+    return redirect('admin_dash', 'admin')
